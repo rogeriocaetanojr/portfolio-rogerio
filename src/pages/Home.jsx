95b3200f0ac1d2b2cb79e2d31fd7ef6a3a316178
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-01 21:24:29 -0300

    feat: cria esqueleto principal da página inicial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 export function Home() {
   return (
-    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <h1>Rogério Caetano</h1>
-      <p>Criando meu portfólio.</p>
+    <main style={{ padding: '2rem', display: 'flex', flexDirection: 'column', gap: '4rem', textAlign: 'center' }}>
+      <section id="inicio">
+        <h1>Rogério Caetano</h1>
+        <p>Criando meu portfólio.</p>
+      </section>
+
+      <section id="sobre">
+        <h2>Sobre Mim</h2>
+      </section>
+
+      <section id="projetos">
+        <h2>Projetos</h2>
+      </section>
+
+      <section id="habilidades">
+        <h2>Habilidades</h2>
+      </section>
+
+      <section id="contato">
+        <h2>Contato</h2>
+      </section>
     </main>
   )
 }
